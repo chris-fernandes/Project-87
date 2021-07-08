df31352d161445b9709953454856a970cff88fb9
chris-fernandes <83762983+chris-fernandes@users.noreply.github.com>
2021-07-08 14:17:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	// to upload images
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '?') 
+	{
+	
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	}
+	
+}
+
